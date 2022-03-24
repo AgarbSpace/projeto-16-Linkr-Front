d62d94ddec1bfceb2a5576e34e4a3ce90d56a3f2
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-03-24 09:30:50 -0300

    FEAT: Add TimelinePage folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import GlobalStyled from "./GlobalStyleds/GlobalStyled";
 import SignUpPage from "./SignUpPage/SignUpPage";
+import TimelinePage from "./TimelinePage/TimelinePage";
 
 export default function App(){
     return (
@@ -9,6 +10,7 @@ export default function App(){
             <GlobalStyled/>
             <Routes>
                 <Route path = "/signup" element = {<SignUpPage/>}/>
+                <Route path = "/timeline" element = {<TimelinePage/>}/>
             </Routes>       
         </BrowserRouter>
     )
