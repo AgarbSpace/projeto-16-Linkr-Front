4824b3505dcbf1fa89c7d937592b5c1c5ecfdfae
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-03-25 17:56:27 -0300

    Feat: rota hashtag filtrando corretamente

--- src/pages/Hashtags/index.js
+++ src/pages/Hashtags/index.js
@@ -16,7 +16,7 @@ import Snippet from "./Styleds/Snippet";
 import Timeline from "./Styleds/Timeline";
 import TimelineContainer from "./Styleds/TimelineContainer";
 import TrendingBox from "./Styleds/TrendingBox";
-import PublishBox from "../../components/PublishBox/index.js";
+import HashtagRanking from "../../components/HashtagRanking/index.js";
 import AvatarImg from "../../components/PublishBox/AvatarPicture";
 import NoPosts from "./Styleds/NoPosts";
 import Loading from "./Styleds/Loading";
@@ -59,14 +59,14 @@ export default function Hashtag() {
       </>
     );
   }
-
+  
   return (
     <>
       <Header />
       <TimelineContainer>
         <Timeline>
           <h2>#{params.hashtag}</h2>
-          {posts.map((post) => (
+          {posts.map(post =>
             <Post>
               <AvatarAndLikeBox>
                 <AvatarImg img={post.picture} />
@@ -85,32 +85,18 @@ export default function Hashtag() {
                 <span>{post.text}</span>
                 <Snippet>
                   <InfosSnippet>
-                    <a href={post.source} target="_blank">
-                      {post.title}
-                    </a>
+                    <a href={post.source} target="_blank">{post.title}</a>
                     <span>{post.description}</span>
-                    <a href={post.source} target="_blank">
-                      {post.source}
-                    </a>
+                    <a href={post.source} target="_blank">{post.source}</a>
                   </InfosSnippet>
-                  <ImageSnippet
-                    src={post.image}
-                    onClick={() => window.open(post.source, "_blank")}
-                  />
+                  <ImageSnippet src={post.image} onClick={() => window.open(post.source, '_blank')} />
                 </Snippet>
               </ContentBox>
             </Post>
-          ))}
+          )}
         </Timeline>
         <TrendingBox>
-          <h1>trending</h1>
-          <hr />
-          <ul>
-            <li>Internet Explorer</li>
-            <li>Opera</li>
-            <li>Firefox</li>
-            <li>Safari</li>
-          </ul>
+          <HashtagRanking />
         </TrendingBox>
       </TimelineContainer>
     </>
