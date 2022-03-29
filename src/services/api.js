378cea189efec983cd40db1081ee71b306c8e385
lostzilla <alanaa92@gmail.com>
2022-03-28 23:20:09 -0300

    change base URL

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { errServer } from "../modals/errServer";
 dotenv.config()
 
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://back--linkr.herokuapp.com"
 
 function createConfig(token) {
   return { headers: { 'Authorization': `Bearer ${token}` } }
